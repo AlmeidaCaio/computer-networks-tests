e9b0f60f17e79167ffe08e69fe4628d2ce1e7268
AlmeidaCaio <almeida.caio1@gmail.com>
2025-02-15 10:58:08 -0300

    Adds OpenSSH server snippet

---
+++ scenario-x/ssh.snippet.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Code Snippet for OpenSSH installation inside the Alpine Container
+#
+# References:
+#    - https://man.openbsd.org/sshd_config
+#    - https://serverfault.com/questions/1015547/what-causes-ssh-error-kex-exchange-identification-connection-closed-by-remote
+#
+#
+ENABLE_SSH_WIDEOPEN="1"
+apk add openrc \
+    openssh  \
+&& [ $ENABLE_SSH_WIDEOPEN == "1" ] && {
+    sed -E -i 's/^(Include\s*\/etc\/ssh\/sshd_config\.d)/#\1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#(Port\s*22)/\1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#(AddressFamily\s*any)/\1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#PermitRootLogin\s*prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#(StrictModes\s*yes))/\1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#MaxAuthTries\s*6/MaxAuthTries 3/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#MaxSessions\s*10/MaxSessions 1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#(PasswordAuthentication\s*yes)/\1/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#PermitEmptyPasswords\s*no/PermitEmptyPasswords yes/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#?AllowTcpForwarding\s*no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config \
+    && sed -E -i 's/^#(UseDNS\s*no)/\1/g' /etc/ssh/sshd_config \
+    && sh -c "echo -e '\nAllowUsers root' >> /etc/ssh/sshd_config" \
+    && sh -c "echo ssh >> /etc/securetty" \
+    && sh -c "passwd < <( echo -e -n '\n\n' )" \
+    && rc-service sshd start
+}
